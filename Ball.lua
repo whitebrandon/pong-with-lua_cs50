0cc1031a4897534f70df9ec4f7dacc6e1114ca3c
white.brandonsean <white.brandonsean@gmail.com>
2020-07-16 18:25:03 -0400

    added new files | refactored

---
+++ Ball.lua
@@ -0,0 +1,46 @@
+Ball = Class{}
+
+function Ball:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+
+    self.dx = math.random(2) == 1 and 100 or -100 
+    self.dy = math.random(-50, 50)
+end
+
+function Ball:reset(x, y)
+    self.x = x-- VIRTUAL_WIDTH / 2 - 2
+    self.y = y-- VIRTUAL_HEIGHT / 2 - 2
+
+    self.dx = math.random(2) == 1 and -100 or 100 -- similar to a ternary
+    self.dy = math.random(-50, 50)
+end
+
+function Ball:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+function Ball:render()
+    -- love.graphics.rectangle( mode, x, y, width, height )
+
+    --[[ 
+        Arguments
+            DrawMode mode
+                How to draw the rectangle.
+            number x
+                The position of top-left corner along the x-axis.
+            number y
+                The position of top-left corner along the y-axis.
+            number width
+                Width of the rectangle.
+            number height
+                Height of the rectangle.
+            Returns
+                Nothing.
+    ]]
+    
+    love.graphics.rectangle('fill', self.x, self.y, 5, 5)
+end
