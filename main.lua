05f9dadee6df949d96d4b2775b5e09c07a53165a
white.brandonsean <white.brandonsean@gmail.com>
2020-07-18 21:16:27 -0400

    added ability to resize

--- main.lua
+++ main.lua
@@ -98,8 +98,12 @@ function love.load()
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         vsync = true,
-        resizable = false,
+        resizable = true,
     })
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
 end
 
 function love.update(dt) -- dt stands for delta time
